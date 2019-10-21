db0180bd95d86fe8ffd294c654b25ae9d02d1394
Jeff Rynhart <jeff.rynhart@du.edu>
2019-10-20 19:51:45 -0600

    Add collections routes

--- search/controller.js
+++ search/controller.js
@@ -4,6 +4,26 @@ const  	config = require('../config/config'),
     	Service = require('./service'),
     	Helper = require('./helper');
 
+exports.collections = function(req, res) {
+	// Service.getCollectionList()
+	res.send("collections");
+}
+
+exports.collection = function(req, res) {
+	// Service.getCollectionData(collectionID)
+	res.send("collection");
+}
+
+exports.collectionItems = function(req, res) {
+	// Service.getCollectionItems(collectionID)
+	res.send("collectionItems");
+}
+
+exports.collectionItem = function(req, res) {
+	// Service.getItemData(collectionID, itemID)
+	res.send("collectionItem");
+}
+
 exports.search = function(req, res) {
 	let query = req.query.q || [""],
 		field = req.query.field || ["all"], 
--- search/routes.js
+++ search/routes.js
@@ -10,6 +10,18 @@
 var Search = require('../search/controller');
 
 module.exports = function (app) {
+	app.route('/collections')
+        .get(Search.collections);
+
+    app.route('/collections/:collection_id')
+        .get(Search.collection);
+
+    app.route('/collections/:id/items')
+        .get(Search.collectionItems);
+
+    app.route('/collections/:collection_id/items/:item_id')
+        .get(Search.collectionItem);
+
     app.route('/search')
         .get(Search.search);
 };
