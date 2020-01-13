6aa0a76a2a56c805dd2636aef1f6fa0ae5ed6a60
Jeff Rynhart <jeff.rynhart@du.edu>
2020-01-13 11:06:42 -0700

    Add format library for metadata fields

---
+++ libs/formatter.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const config = require("../config/config");
+
+exports.getRepositoryResourceUrl = function(metadataObject) {
+	if(metadataObject["Resource URI"]) {
+		metadataObject["Resource URI"] = config.repositoryDomain + "/" + metadataObject["Resource URI"];
+	}
+}
