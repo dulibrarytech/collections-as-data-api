476043fe04fab466949b38c4d1c556ff76d9ee1d
Jeff Rynhart <jeff.rynhart@du.edu>
2020-06-28 14:50:14 -0600

    Add ejs init, set views and public assets folders

--- config/express.js
+++ config/express.js
@@ -12,13 +12,17 @@ var express = require('express'),
 module.exports = function () {
     var app = express();
 
-    if (process.env.NODE_ENV === 'prod') {
-        app.use(compress());
+    if (process.env.NODE_ENV === 'production') {
+        app.use(compression());
     }
 
-    require('../collections/routes.js')(app);
-    //require('../collections/routes/api_v1.js')(app);
+    app.use(express.static('./public'));
+    app.set('views', './views');
+    app.set('view engine', 'ejs');
 
+    require('../collections/routes.js')(app);
+    require('../api-form/routes.js')(app);
+    //require('../collections/routes/api_v1.js')(app);
     require('../search/routes.js')(app);
 
     return app;
