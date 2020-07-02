057d2d4cdddfbdf9cc407a455f14e01f1691662a
Jeff Rynhart <jeff.rynhart@du.edu>
2020-07-01 23:28:56 -0600

    Remove all dev comments and logs

--- public/libs/CadApiForm.js
+++ public/libs/CadApiForm.js
@@ -7,7 +7,6 @@ var CadApiForm = (function() {
 	var submitGetRequest;
 	var onSelectParam;
 
-	var getUri;
 	var ajaxRequest;
 	var resetDisplays;
 	var resetUriParam;
@@ -48,35 +47,29 @@ var CadApiForm = (function() {
 				if(param.depends_on == null) {
 					// Create a select box
 					if(!document.getElementById(param.name + "_select")) {
-						console.log("TEST creating select box for", param)
 						let formGroup = document.createElement("DIV");
 						formGroup.classList.add("form-group");
 						formGroup.classList.add("param-select");
 						let select = document.createElement("SELECT");
 						select.setAttribute("id", param.name + "_select")
 						select.classList.add("form-control");
-						//select.setAttribute("onmousedown", "CadApiForm.resetUriParam(this)");
 						select.setAttribute("onchange", "CadApiForm.onSelectParam(this)");
 						formGroup.appendChild(select);
-						//document.getElementsByClassName("api-form-section")[0].appendChild(formGroup);
 						document.getElementById("param-options").appendChild(formGroup);
-							console.log("TEST Aparam is", param)
-						// Get data for box
+
+						// Get data for the select box
 						let url = config.apiDomain + param.data;
 						ajaxRequest("get", url, function(error, status, response) {
 							if(error) {
 								console.log(error);
 							}
 							else {
-									console.log("TEST ajax response Bparam is", param)
 								let responseObject = JSON.parse(response),
 									map = param.option_map, option, item;
 
 								// Create placeholder text
 								option = document.createElement("OPTION");
 								option.setAttribute("value", "");
-									console.log("TEST Cparam is", param)
-									console.log("TEST label is", param.label)
 								option.innerHTML = param.label;
 								select.appendChild(option);
 
@@ -99,6 +92,7 @@ var CadApiForm = (function() {
 	submitGetRequest = function() {
 		let uri = document.getElementById("query-display").value,
 		 	url = config.apiDomain + uri;
+
 		ajaxRequest("get", url, function(error, status, response) {
 			if(error) {
 				console.log(error);
@@ -112,10 +106,8 @@ var CadApiForm = (function() {
 	}
 
 	resetUriParam = function(selectBox) {
-			console.log("TEST reset uri param")
 		// Replace the param value in the query uri with the brace delimited param name
 		let paramName = selectBox.id.replace("_select", "");
-		console.log("TEST param cache", cache[paramName])
 		let param = "{" + paramName + "}";
 		if(selectBox.value) {	
 			document.getElementById("query-display").value = document.getElementById("query-display").value.replace(cache[paramName], param);
@@ -129,35 +121,33 @@ var CadApiForm = (function() {
 		let paramName = selectBox.id.replace("_select", ""),
 			queryString = document.getElementById("query-display").value,
 			paramPlaceholder = "{" + paramName + "}";
+
 		queryString = queryString.replace(paramPlaceholder, selectBox.value);
 		document.getElementById("query-display").value = queryString;
 		cache[paramName] = selectBox.value;
 		clearDependentParams(selectBox);
 
-		// Check for a dependant param
+		// Check for a dependent param
 		let endpointId = document.getElementById("endpoint-select").value, param;
 		for(var index in config.apiFormEndpoints[endpointId].params) {
 			param = config.apiFormEndpoints[endpointId].params[index];
 			if(param.depends_on == paramName) {
-				// Insert a select box (TODO func add ParamSelectBox(param)
+				// Insert a select box (TODO func add ParamSelectBox(param))
 				let formGroup = document.createElement("DIV");
 				formGroup.classList.add("form-group");
 				formGroup.classList.add("param-select");
 				let select = document.createElement("SELECT");
 				select.setAttribute("id", param.name + "_select")
 				select.classList.add("form-control");
-				//select.setAttribute("onmousedown", "CadApiForm.resetUriParam(this)");
 				select.setAttribute("onchange", "CadApiForm.onSelectParam(this)");
 				formGroup.appendChild(select);
-				//document.getElementsByClassName("api-form-section")[0].appendChild(formGroup);
 				document.getElementById("param-options").appendChild(formGroup);
-				// Use data uri to fetch data for the select, add options for each response item
+
 				// Get data for box TODO func populateParamSelectBox(param)
 				let url = config.apiDomain + param.data,
 					paramPlaceholder = "{" + paramName + "}";
 					url = url.replace(paramPlaceholder, selectBox.value);
 
-					console.log("TEST ajax url", url);
 				ajaxRequest("get", url, function(error, status, response) {
 					if(error) {
 						console.log(error);
@@ -182,29 +172,18 @@ var CadApiForm = (function() {
 						}
 					}
 				});
-				// Add id=param id (match param name)	
-				console.log("TEST found dependent param", config.apiFormEndpoints[endpointId].params[index])	
-
 			}
 		}
-		console.log("TEST cur endpoint id", document.getElementById("endpoint-select").value, paramName);
 	}
 
 	clearDependentParams = function(selectBox) {
 		while(selectBox.parentNode.nextSibling) {
-			console.log("TEST removing sibling", selectBox.parentNode.nextSibling.firstChild.value.length)
 			if(selectBox.parentNode.nextSibling.firstChild.value.length > 0) {
 				let paramPlaceholder = "{" + selectBox.parentNode.nextSibling.firstChild.id.replace("_select", "") + "}";
-				console.log("TEST appendstring pre", document.getElementById("query-display").value)
 				document.getElementById("query-display").value = document.getElementById("query-display").value.replace(selectBox.parentNode.nextSibling.firstChild.value, paramPlaceholder)
-				console.log("TEST appendstring post", document.getElementById("query-display").value)
 			}
 			selectBox.parentNode.nextSibling.remove();
 		}
-	}
-
-	getUri = function(uri, params) {
-
 	}
 
 	ajaxRequest = function(type, url, callback) {
@@ -223,15 +202,10 @@ var CadApiForm = (function() {
 	}
 
 	resetDisplays = function() {
-			console.log("TEST reset");
 		document.getElementById("query-display").value = "";
 		document.getElementById("query-response-display").value = "";
 		document.getElementById("param-options").innerHTML = "";
-		// let paramSelects = document.getElementsByClassName("param-select");
-		// for(var i=0; i<paramSelects.length; i++) {
-		// 	console.log("TEST removing", paramSelects[i])
-		// 	paramSelects[i].remove();
-		// }
+		cache = {};
 	}
 
 	return {
