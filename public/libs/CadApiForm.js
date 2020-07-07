d33449d0e508522aebba606826070470e5f84f68
Jeff Rynhart <jeff.rynhart@du.edu>
2020-07-06 19:18:28 -0600

    Add checkbox list to select multiple items, for multiple select params, update query response to display multiple query strings, refactor form display control functions

--- public/libs/CadApiForm.js
+++ public/libs/CadApiForm.js
@@ -6,22 +6,26 @@ var CadApiForm = (function() {
 	var onSelectEndpointOption;
 	var submitGetRequest;
 	var onSelectParam;
+	var onCheckParam;
 
 	var ajaxRequest;
 	var resetDisplays;
+	var refreshQueryDisplay;
 	var resetUriParam;
 	var clearDependentParams;
+	var addParameterSelectBox;
+	var getUrlParamValues;
 
 	var config;
-	var cache = {};
+	var cache;
 
 	initForm = function(configObject) {
 		var endpoint,
 			option,
 		    select = document.getElementById("endpoint-select");
 
-		// Set global config object
 		config = configObject;
+		cache = {};
 		resetDisplays();
 
 		// Add an option to the select box for each endpoint
@@ -36,6 +40,7 @@ var CadApiForm = (function() {
 	}
 
 	onSelectEndpointOption = function(endpointId) {
+		cache = {};
 		resetDisplays();
 		let endpoint = config.apiFormEndpoints[endpointId];
 		document.getElementById("query-display").value = endpoint.uri;
@@ -45,51 +50,16 @@ var CadApiForm = (function() {
 			for(var param of endpoint.params) {
 				// this is the first parameter
 				if(param.depends_on == null) {
-					// Create a select box
-					if(!document.getElementById(param.name + "_select")) {
-						let formGroup = document.createElement("DIV");
-						formGroup.classList.add("form-group");
-						formGroup.classList.add("param-select");
-						let select = document.createElement("SELECT");
-						select.setAttribute("id", param.name + "_select")
-						select.classList.add("form-control");
-						select.setAttribute("onchange", "CadApiForm.onSelectParam(this)");
-						formGroup.appendChild(select);
-						document.getElementById("param-options").appendChild(formGroup);
-
-						// Get data for the select box
-						let url = config.apiDomain + param.data;
-						ajaxRequest("get", url, function(error, status, response) {
-							if(error) {
-								console.log(error);
-							}
-							else {
-								let responseObject = JSON.parse(response),
-									map = param.option_map, option, item;
-
-								// Create placeholder text
-								option = document.createElement("OPTION");
-								option.setAttribute("value", "");
-								option.innerHTML = param.label;
-								select.appendChild(option);
-
-								// Add an option for each item in the response
-								for(var index in responseObject.data) {
-									item = responseObject.data[index];
-									option = document.createElement("OPTION");
-									option.setAttribute("value", item[map.value]);
-									option.innerHTML = item[map.text];
-									select.appendChild(option);
-								}
-							}
-						});
-					}
+					addParameterSelectBox(param);
 				}
 			} 
 		}
 	}
 
 	submitGetRequest = function() {
+		// TODO: if qdisp-val contains "\n\n" delimiter (ad to cfg), explode on delimiter
+		// loop array, ajax then append response to qresponse string (display value string)
+
 		let uri = document.getElementById("query-display").value,
 		 	url = config.apiDomain + uri;
 
@@ -105,56 +75,116 @@ var CadApiForm = (function() {
 		});
 	}
 
-	resetUriParam = function(selectBox) {
-		// Replace the param value in the query uri with the brace delimited param name
-		let paramName = selectBox.id.replace("_select", "");
-		let param = "{" + paramName + "}";
-		if(selectBox.value) {	
-			document.getElementById("query-display").value = document.getElementById("query-display").value.replace(cache[paramName], param);
+	resetUriParam = function(paramName) {
+		let endpointId = document.getElementById("endpoint-select").value;
+		if(cache[paramName]) {
+			delete cache[paramName];
 		}
+		document.getElementById("query-display").value = getUrlParamValues(config.apiFormEndpoints[endpointId].uri);
 	}
 
 	onSelectParam = function(selectBox) {
-		resetUriParam(selectBox);
-		// Replace the brace delimited param name in the query uri with the selected param value
-		document.getElementById("query-response-display").value = "";
-		let paramName = selectBox.id.replace("_select", ""),
-			queryString = document.getElementById("query-display").value,
-			paramPlaceholder = "{" + paramName + "}";
-
-		queryString = queryString.replace(paramPlaceholder, selectBox.value);
-		document.getElementById("query-display").value = queryString;
-		cache[paramName] = selectBox.value;
+		let paramName = selectBox.id.replace("_select", "")
 		clearDependentParams(selectBox);
+		document.getElementById("query-response-display").value = "";
+		cache[paramName] = selectBox.value;
+		refreshQueryDisplay();
 
 		// Check for a dependent param
 		let endpointId = document.getElementById("endpoint-select").value, param;
 		for(var index in config.apiFormEndpoints[endpointId].params) {
 			param = config.apiFormEndpoints[endpointId].params[index];
 			if(param.depends_on == paramName) {
-				// Insert a select box (TODO func add ParamSelectBox(param))
-				let formGroup = document.createElement("DIV");
-				formGroup.classList.add("form-group");
-				formGroup.classList.add("param-select");
-				let select = document.createElement("SELECT");
-				select.setAttribute("id", param.name + "_select")
-				select.classList.add("form-control");
-				select.setAttribute("onchange", "CadApiForm.onSelectParam(this)");
-				formGroup.appendChild(select);
-				document.getElementById("param-options").appendChild(formGroup);
+				addParameterSelectBox(param);
+			}
+		}
+	}
 
-				// Get data for box TODO func populateParamSelectBox(param)
-				let url = config.apiDomain + param.data,
-					paramPlaceholder = "{" + paramName + "}";
-					url = url.replace(paramPlaceholder, selectBox.value);
+	onCheckParam = function(checkBox) {
+		let endpointId = document.getElementById("endpoint-select").value,
+			uri = config.apiFormEndpoints[endpointId].uri,
+			paramName = checkBox.name;
 
-				ajaxRequest("get", url, function(error, status, response) {
-					if(error) {
-						console.log(error);
+		uri = uri.replace("{" + paramName + "}", checkBox.value) + "\n\n";
+		uri = getUrlParamValues(uri);
+
+		if(checkBox.checked) {
+			if(!cache[paramName] || typeof cache[paramName] == "string") {
+				cache[paramName] = [];
+			}
+			cache[paramName].push(checkBox.value);
+
+			if(document.getElementById("query-display").value.indexOf("{" + paramName + "}") >= 0) {
+				document.getElementById("query-display").value = "";
+			}
+			document.getElementById("query-display").value += uri;
+		}	
+		else {
+			// Remove string from cache array
+			if(cache[paramName] && typeof cache[paramName] == "object") {
+				let paramValues = cache[paramName] || [];
+				cache[paramName].splice(paramValues.indexOf(checkBox.value),1)
+			}
+
+			// Replace uri in qdispl string with ""
+			document.getElementById("query-display").value = document.getElementById("query-display").value.replace(uri, "");
+			if(document.getElementById("query-display").value.length == 0) {
+				refreshQueryDisplay();
+			}
+		}	
+	}
+
+	addParameterSelectBox = function(param) {
+		if(!document.getElementById(param.name + "_select")) {
+			let formGroup = document.createElement("DIV");
+			formGroup.classList.add("form-group");
+			formGroup.classList.add("param-select");
+
+			let url = config.apiDomain + getUrlParamValues(param.data);
+			ajaxRequest("get", url, function(error, status, response) {
+				if(error) {
+					console.log(error);
+				}
+				else {
+					let responseObject = JSON.parse(response),
+						map = param.option_map;
+
+					if(param.multi_select) {
+						// Populate with checkboxes (onclick onCheckParam(checkBox))
+						let container = document.createElement("DIV");
+						container.classList.add("list-container");
+						container.classList.add("form-control");
+						let list = document.createElement("UL"), listItem, checkbox, label, item;
+						list.setAttribute("id", param.name + "_select");
+
+						// Add the response items
+						for(var index in responseObject.data) {
+							item = responseObject.data[index];
+							listItem = document.createElement("LI");
+							checkbox = document.createElement("INPUT");
+							checkbox.setAttribute("type", "checkbox");
+							checkbox.setAttribute("onclick", "CadApiForm.onCheckParam(this)");
+							checkbox.setAttribute("name", param.name);
+							checkbox.setAttribute("value", item[map.value]);
+							label = document.createElement("LABEL");
+							label.innerHTML = item[map.text];
+							listItem.appendChild(checkbox);
+							listItem.appendChild(label);
+							list.appendChild(listItem);
+						}
+
+						container.appendChild(list);
+						formGroup.appendChild(container);
+						document.getElementById("param-options").appendChild(formGroup);
 					}
 					else {
-						let responseObject = JSON.parse(response),
-							map = param.option_map, option, item;
+						// Add select box
+						let select = document.createElement("SELECT"), option, item;
+						select.setAttribute("id", param.name + "_select")
+						select.classList.add("form-control");
+						select.setAttribute("onchange", "CadApiForm.onSelectParam(this)");
+						formGroup.appendChild(select);
+						document.getElementById("param-options").appendChild(formGroup);
 
 						// Create placeholder text
 						option = document.createElement("OPTION");
@@ -171,15 +201,31 @@ var CadApiForm = (function() {
 							select.appendChild(option);
 						}
 					}
-				});
-			}
+				}
+			});
 		}
 	}
 
+	getUrlParamValues = function(url) {
+		let params = url.match(/{(.*?)\}/g), paramName;
+		if(params) {
+			for(var param of params) {
+				paramName = param.replace("{", "").replace("}", "");
+				if(cache[paramName] && typeof cache[paramName] == "string") {
+					url = url.replace(param, cache[paramName]);
+				}
+			}
+		}
+		return url;
+	}
+
 	clearDependentParams = function(selectBox) {
+		document.getElementById("query-display").value = getUrlParamValues(document.getElementById("query-display").value);
 		while(selectBox.parentNode.nextSibling) {
-			if(selectBox.parentNode.nextSibling.firstChild.value.length > 0) {
-				let paramPlaceholder = "{" + selectBox.parentNode.nextSibling.firstChild.id.replace("_select", "") + "}";
+			if(selectBox.parentNode.nextSibling.firstChild.value && selectBox.parentNode.nextSibling.firstChild.value.length > 0) {
+				let paramName = selectBox.parentNode.nextSibling.firstChild.id.replace("_select", "");
+				let paramPlaceholder = "{" + paramName + "}";
+				resetUriParam(paramName);
 				document.getElementById("query-display").value = document.getElementById("query-display").value.replace(selectBox.parentNode.nextSibling.firstChild.value, paramPlaceholder)
 			}
 			selectBox.parentNode.nextSibling.remove();
@@ -205,7 +251,12 @@ var CadApiForm = (function() {
 		document.getElementById("query-display").value = "";
 		document.getElementById("query-response-display").value = "";
 		document.getElementById("param-options").innerHTML = "";
-		cache = {};
+	}
+
+	refreshQueryDisplay = function() {
+		let endpointId = document.getElementById("endpoint-select").value;
+		let uri = config.apiFormEndpoints[endpointId].uri;
+		document.getElementById("query-display").value = getUrlParamValues(uri);
 	}
 
 	return {
@@ -218,11 +269,14 @@ var CadApiForm = (function() {
 		submitGetRequest: function() {
 			return submitGetRequest();
 		},
-		resetUriParam: function(selectBox) {
-			return resetUriParam(selectBox);
+		resetUriParam: function(paramName) {
+			return resetUriParam(paramName);
 		},
 		onSelectParam: function(selectBox) {
 			return onSelectParam(selectBox);
+		},
+		onCheckParam: function(checkBox) {
+			onCheckParam(checkBox);
 		}
 	}
 })()
